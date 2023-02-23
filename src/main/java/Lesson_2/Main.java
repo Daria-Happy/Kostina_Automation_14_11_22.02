0277164359263c330ac657d5b3ee25b979fce847
Daria-Happy <kostina2206@gmail.com>
2023-02-23 11:54:52 +0200

    test

--- src/main/java/Lesson_2/Main.java
+++ src/main/java/Lesson_2/Main.java
@@ -1,2 +1,9 @@
-package Lesson_2;public class main {
+package Lesson_2;
+
+public class Main {
+    public static void main (String [] args){
+        System.out.println("Hello World!");
+        System.out.println("Hello World!!!!!");
+        System.out.println("Hello World!!!!!");
+    }
 }
